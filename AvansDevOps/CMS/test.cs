e6d445cc7d3e5aca167d7e3adf253d60e4084101
Justnuked <thommartens14@hotmail.com>
2019-02-21 18:52:28 +0100

    added basic project structure

---
+++ AvansDevOps/CMS/test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS
+{
+    public class test
+    {
+        public static void testMethod()
+        {
+            Console.WriteLine("testFromCMS");
+        }
+    }
+}
