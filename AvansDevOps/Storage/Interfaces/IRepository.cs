cbf4d6293c27851b562a6648a23c65ec943e4ac5
Justnuked <thommartens14@hotmail.com>
2019-02-21 19:04:23 +0100

    added generic storage interface

---
+++ AvansDevOps/Storage/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storage.Interfaces
+{
+    interface IRepository<Entity> where Entity : class
+    {
+        void Save(Entity e);
+        void Delete(Entity e);
+        void Update(int id, Entity newE);
+        List<Entity> GetAll();
+        Entity GetById();
+
+    }
+}
