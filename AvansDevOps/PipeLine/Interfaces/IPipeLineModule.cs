625e6929aa8f95544f660c433d536db1118194df
Justnuked <thommartens14@hotmail.com>
2019-02-21 22:20:12 +0100

    added pipeline logic and observable

---
+++ AvansDevOps/PipeLine/Interfaces/IPipeLineModule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PipeLine.Classes;
+
+namespace PipeLine.Interfaces
+{
+    public interface IPipeLineModule
+    {
+        void Execute();
+        Status GetStatus();
+    }
+}
---
+++ AvansDevOps/Utilities/Observable/Observable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities.Observable
+{
+    public class Observable
+    {
+        private List<Observer> observers = new List<Observer>();
+
+        public void Subscribe(Observer o)
+        {
+            observers.Add(o);
+        }
+
+        public void UnSubscribe(Observer o)
+        {
+            observers.Remove(o);
+        }
+
+        public void Notify(string message)
+        {
+            observers.ForEach(x => x.Update(message));
+        }
+    }
+}
